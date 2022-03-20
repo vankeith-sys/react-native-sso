0164e7169e43fbbf865c64b9c0739f015ebfd8d1
Van Keith <vkalmazan@gmail.com>
2022-03-20 21:21:23 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Text>Holy shet, this is my first react native app.</Text>
+      <Text>I'm fucking amazing</Text> */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Unified Single Sign On</Text>
+        <View style={styles.items}>
+          {/* This is where the tasks will go. */}
+          <Task text={'Log In with Google'}/>
+          <Task text={'Log In with Facebook'}/>
+          <Task text={'Log In with Apple'}/>
+          <Task text={'Log In with Email'}/>
+
+          
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +26,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop: 30,
   },
 });
